4e3bbdf1ddfc69a6443db5749b045c8d4c9e3e4d
mavjz28 <maja.miaskiewicz@gmail.com>
2024-05-05 19:39:17 +0200

    feat: add time of get req

--- index.ts
+++ index.ts
@@ -2,8 +2,13 @@ import axios from 'axios';
 
 const main = async (x: number) => {
     for (let i = x; i > 0; i--) {
+        const timeStart = new Date();
+
         const res = await axios.get('https://tvgo.orange.pl/gpapi/status');
-        console.log(res.data);
+
+        const timeEnd = new Date();
+        const timeDiff = (timeEnd.getTime() - timeStart.getTime()) / 1000;
+        console.log(timeDiff);
     }
 };
 
