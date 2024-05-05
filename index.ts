111ee829ebda0f23880fac409ca6abf4bd42d6b1
mavjz28 <maja.miaskiewicz@gmail.com>
2024-05-05 19:44:39 +0200

    feat: add checking status of req

--- index.ts
+++ index.ts
@@ -1,14 +1,32 @@
 import axios from 'axios';
+import moment from 'moment';
 
 const main = async (x: number) => {
     for (let i = x; i > 0; i--) {
         const timeStart = new Date();
 
-        const res = await axios.get('https://tvgo.orange.pl/gpapi/status');
+        const res = await axios.get('https://tvgo.orange.pl/gpapi/status', {
+            validateStatus: (status) => {
+                return status > 99;
+            },
+        });
+
+        if (res.status !== 200) {
+            console.error('HTTP status no. ' + res.status);
+        }
 
         const timeEnd = new Date();
         const timeDiff = (timeEnd.getTime() - timeStart.getTime()) / 1000;
-        console.log(timeDiff);
+        const connectionData =
+            moment(timeStart).format('DD.MM.YYYY HH:mm:ss') +
+            '; ' +
+            res.status +
+            ' ' +
+            res.statusText +
+            '; ' +
+            timeDiff +
+            's;\r\n';
+        console.log(connectionData);
     }
 };
 
